5427e45d4ada10bc5b7a63a21d3912be69095da9
Edgars Meirans <edgarsmeirans@gmail.com>
2021-04-20 16:34:29 +0300

    +

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,39 @@ window.addEventListener('load', () => {
     console.log(produkti)
     render();
 });
+
+document.getElementById('jaunsProdukts').addEventListener('click', () => {
+    POP_UP.style.display = 'block';
+
+})
+
+document.getElementById('pievienotProduktu').addEventListener('click', () => {
+    POP_UP.style.display = 'none';
+    let produkti = {produkts: produkts.value, daudzums: daudzums.value};
+
+    produkts.value = "";
+    daudzums.value = "";
+
+    produkti.push(produkts);
+
+    render();
+
+})
+
+function render() {
+    let saraksts = document.getElementById('saraksts');
+    saraksts.innerHTML = "";
+
+    for(let i = 0; i < produkti.length; i++) {
+        let produkts =
+        <div class="produkts">
+            <h3>Produkts: ${gramatas[i].produkts</h3>
+            <h4>Daudzums: ${gramatas[i].daudzums</h4>
+        </div>
+
+        saraksts.innerHTML += produkts;
+
+    }
+
+localStorage.setItem("produkti", JSON.stringify(produkti))
+}
